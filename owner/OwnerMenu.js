198562fa8950ecb0500776ea133376ad7a82012b
Daniel-Elgarisi <danielelgarisi7@gmail.com>
2022-01-02 23:18:04 +0200

    added Front

---
+++ owner/OwnerMenu.js
@@ -0,0 +1,40 @@
+function nav(choice) {
+    let res;
+    switch (choice) {
+        case 1:
+            res = "navigation/HomePage/home.html"
+
+            break;
+        case 2:
+            res = "../EditProfile/EditProfile.html"
+
+            break;
+        case 3:
+            res = "navigation/Emergancy/Emergancy.html"
+            break;
+        case 4:
+            res = "navigation/Appointment/Appointment.html"
+            break;
+        case 5:
+            res = "navigation/DeleteAppointment/DelAppointment.html"
+            break;
+        case 6:
+            res = "navigation/ViewMedicalReport/ViewMedicalReport.html"
+            break;
+        case 7:
+            res = "navigation/Vaccines/Vaccines.html"
+            break;
+        case 8:
+            res = "navigation/PhotoAlbum/PhotoAlbum.html"
+            break;
+        case 9:
+            sessionStorage.setItem("user", null)
+            sessionStorage.setItem("role", null)
+            window.location.href = '../index.html';
+            break;
+
+    }
+    let frame = document.getElementById("frame")
+    frame.setAttribute("src", res)
+
+}
